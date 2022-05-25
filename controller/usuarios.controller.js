994e893ba16b5026d8894e1733430a544f89232d
Juan David De la hoz <hozjuanda11@gmail.com>
2022-05-24 23:38:21 -0500

    put y get terminado

--- controller/usuarios.controller.js
+++ controller/usuarios.controller.js
@@ -4,24 +4,34 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario.model');
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = async(req, res = response) => {
 
-    const { q, nombre='No name', apiKey, page = 1, limit } = req.query;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado : true }
+
+    // const usuarios = await Usuario.find( query )
+    //     .limit( Number(limite) )
+    //     .skip( Number(desde) )
+
+    // const total = await Usuario.countDocuments( query );
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments( query ),
+        Usuario.find( query )
+            .limit(Number(limite))
+            .skip(Number(desde))
+    ]);
 
     res.json({
-        msg: 'get API - Controlador',
-        q,
-        nombre,
-        apiKey,
-        page,
-        limit
+        total,
+        usuarios
     })
 }
 
 const usuariosPut = async(req, res = response) => {
 
     const { id } = req.params;
-    const { password, google, correo, ...resto } = req.body;
+    const { _id, password, google, correo, ...resto } = req.body;
 
     // TODO validat contra base de datos
     if ( password ) {
@@ -32,10 +42,7 @@ const usuariosPut = async(req, res = response) => {
 
     const usuario = await Usuario.findByIdAndUpdate( id, resto );
 
-    res.status(500).json({
-        msg: 'put API - Controlador',
-        usuario
-    })
+    res.status(500).json(usuario)
 }
 
 const usuariosPost = async(req, res = response) => {
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRoleValido, emailExiste } = require('../helpers/db-validators');
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const { usuariosGet, 
         usuariosPut, 
@@ -15,7 +15,12 @@ const router = Router();
 
 router.get('/', usuariosGet);
 
-router.put('/:id', usuariosPut);
+router.put('/:id', [
+        check('id', 'No es un ID válido').isMongoId(),
+        check('id').custom( existeUsuarioPorId ),
+        check( 'rol' ).custom( esRoleValido ),
+        validarCampos
+], usuariosPut);
 
 router.post('/', [
         check( 'nombre', 'El nombre es obligatorio' ).not().isEmpty(),
