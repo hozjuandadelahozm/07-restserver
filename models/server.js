5d1bcfa38b233a8a752f05c8a8b189d4fd56b3fc
Juan David De la hoz <hozjuanda11@gmail.com>
2022-06-06 22:59:02 -0500

    Inicio implemetacion autenticacion con JWT

--- models/server.js
+++ models/server.js
@@ -8,7 +8,8 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usuariosPath = '/api/usuarios'
+        this.usuariosPath = '/api/usuarios';
+        this.authPath     = '/api/auth';
 
         // Conectar a base de datos de
         this.conectarDB();
@@ -39,8 +40,8 @@ class Server {
 
     routes() {
         
+        this.app.use( this.authPath, require('../routes/auth.routes'));
         this.app.use( this.usuariosPath, require('../routes/usuarios.routes'));
-
     }
     
     listen() {
